36745c785eb2a6174f9444f07103413c17a6f940
Yianni Moustakas <yianni.m13@gmail.com>
2018-11-28 17:41:25 +1100

    apikey hidden

--- app/javascript/packs/components/MapContainer.jsx
+++ app/javascript/packs/components/MapContainer.jsx
@@ -3,12 +3,12 @@ import MapConfig from './MapConfig';
 
 class MapContainer extends Component{
   render(){
-    const google_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCYA7hXRCqJ0d9ZAnNzSW_gd05GcNO6AGc&v=3'
+    const google_URL = GOOGLE_MAPS_API_KEY
 
 		return (
 			<MapConfig
 				items={this.props.items}
-				googleMapURL={google_URL}
+				googleMapURL={GOOGLE_MAPS_API_KEY}
 				loadingElement={<div style={{ height: `100vh` }} />}
 				containerElement={<div style={{ height: '400px' }}></div>}
 				mapElement={<div style={{ height: `100%` }} />}
